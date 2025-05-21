7e628e51a75db63b3ac454d31e6342fe471dee37
CodeByNavin <bedinavinyt@gmail.com>
2025-05-20 22:32:51 -0400

    FIxing route issue??

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import MainPage from './main/App';
 import Game from './game/App';
 
 export default function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/game" element={<Game />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </HashRouter>
   )
 };
