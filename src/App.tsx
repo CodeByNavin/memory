1a39d68d8101541f1a340a5dd3645263d24d14f0
CodeByNavin <bedinavinyt@gmail.com>
2025-05-20 22:36:26 -0400

    ^^

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { Route, Routes, HashRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import MainPage from './main/App';
 import Game from './game/App';
 
 export default function App() {
 
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/game" element={<Game />} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/game" element={<Game />} />
+    </Routes>
   )
 };
